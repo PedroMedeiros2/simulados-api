eda76b192a2d0a6b5473e73da6160363e4a14cb3
Pedro Henrique Soares Medeiros <111461491+PedroMedeiros2@users.noreply.github.com>
2025-05-14 16:18:25 -0300

    feat: configura dependências iniciais do projeto Spring Boot e Maven

---
+++ src/main/java/io/github/pedromedeiros2/simulados_api/SimuladosApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedromedeiros2.simulados_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladosApiApplication.class, args);
+	}
+
+}
